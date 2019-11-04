5c6e71d73b7296308fdcf492fb2d9c507af79c24
Christina Chen <cyc25@duke.edu>
2019-11-05 00:07:48 +0100

    fix minor bug with animation for last section

--- src/components/Sections/Resume.js
+++ src/components/Sections/Resume.js
@@ -12,11 +12,8 @@ export default class Resume extends Component {
         return (
             <div data-aos="zoom-in-up"
                  data-aos-duration="1000"
-                 data-aos-easing="ease-in-out"
-                 data-aos-offset="1500"
-                 data-aos-mirror="true"
+                 data-aos-offset="1000"
                  data-aos-once="false"
-                 data-aos-anchor-placement="top-center"
                  className="about-me">
                 <p className="section-title">Resume</p>
                 <div className="section-items">
